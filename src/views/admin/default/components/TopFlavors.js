418480070b0828a8cbf3a51c5a396d1c24d304a5
wxyn <100791680+XiaoyananWang@users.noreply.github.com>
2023-10-11 14:55:00 -0400

    Deleted Searchbars

--- src/views/admin/default/components/TopFlavors.js
+++ src/views/admin/default/components/TopFlavors.js
@@ -1,16 +1,10 @@
-import React, { useState, Fragment } from "react";
+import React, { useState } from "react";
 import Card from "components/card";
-import { Menu, Transition } from "@headlessui/react";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
 import flavorDefault from "assets/img/dashboards/flavorDefault.png";
 import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
-
 const TopFlavors = () => {
-  const [selectedTimePeriod, setSelectedTimePeriod] = useState("Time Period");
+
   const renderRating = (totalStars, activeStars) => {
     const stars = [];
     for (let i = 0; i < totalStars; i++) {
@@ -25,47 +19,10 @@ const TopFlavors = () => {
 
   return (
     <Card extra="flex flex-col bg-white w-full rounded-3xl py-6 px-2 text-left h-96">
-      <div className="mb-auto flex items-center justify-between px-6">
+      <div className="flex items-center justify-between px-6">
         <h2 className="text-lg font-bold text-gray-900 dark:text-white">
           Top 5 Flavor
         </h2>
-        <Menu as="div" className="relative inline-block text-left">
-          <div>
-            <Menu.Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-              {selectedTimePeriod}
-              <ChevronDownIcon
-                className="-mr-1 h-5 w-5 text-gray-400"
-                aria-hidden="true"
-              />
-            </Menu.Button>
-          </div>
-          <Transition as={Fragment}>
-            <Menu.Items className="ring-black absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-opacity-5 focus:outline-none">
-              <div className="py-1">
-                {["Daily", "Monthly", "Quarterly", "Yearly"].map(
-                  (timePeriod) => (
-                    <Menu.Item key={timePeriod}>
-                      {({ active }) => (
-                        <a
-                          href="#"
-                          className={classNames(
-                            active
-                              ? "bg-gray-100 text-gray-900"
-                              : "text-gray-700",
-                            "block px-4 py-2 text-sm"
-                          )}
-                          onClick={() => setSelectedTimePeriod(timePeriod)}
-                        >
-                          {timePeriod}
-                        </a>
-                      )}
-                    </Menu.Item>
-                  )
-                )}
-              </div>
-            </Menu.Items>
-          </Transition>
-        </Menu>
       </div>
       <div className="mt-5 space-y-3 px-6">
         {[1, 2, 3, 4, 5].map((_, index) => (
