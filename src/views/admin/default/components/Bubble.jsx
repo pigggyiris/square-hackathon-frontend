362b37d648a1f7143805590cb1ad29d8f6008e2f
pigggyiris <95093482+pigggyiris@users.noreply.github.com>
2023-10-11 19:49:58 -0400

    calender

--- src/views/admin/default/components/Bubble.jsx
+++ src/views/admin/default/components/Bubble.jsx
@@ -1,17 +1,33 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Transition } from "@headlessui/react";
 
 const Bubble = ({ title, content, color }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const bubbleRef = useRef(null);
+
+  const handleMouseEnter = () => {
+    if (bubbleRef.current) {
+      bubbleRef.current.classList.add("bubble-animation");
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (bubbleRef.current) {
+      bubbleRef.current.classList.remove("bubble-animation");
+    }
+    setIsOpen(false);
+  };
 
   return (
     <div
       className="relative flex-grow p-2"
-      onMouseLeave={() => setIsOpen(false)}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
     >
       <button
+        ref={bubbleRef}
         onClick={() => setIsOpen(!isOpen)}
-        className={`ml-3 flex items-center justify-center rounded-xl px-4 py-2 text-sm text-white shadow-lg hover:bg-opacity-90 ${color} bubble-animation w-full rounded`}
+        className={`ml-3 flex items-center justify-center rounded-xl px-4 py-2 text-sm font-bold text-white shadow-lg ${color} w-full rounded`}
       >
         {title}
       </button>
@@ -24,7 +40,7 @@ const Bubble = ({ title, content, color }) => {
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
       >
-        <div className="absolute left-0 top-[-100%] z-10 ml-16 mt-12 w-72 rounded-lg border bg-white p-4 shadow-xl">
+        <div className="absolute left-0 top-[-100%] z-10 ml-16 mt-12 w-72 rounded-lg border bg-fall-50 p-4 shadow-xl">
           <p className="text-black">{content}</p>
         </div>
       </Transition>
