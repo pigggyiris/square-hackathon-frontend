ba16cad3e42aa1770effb5397e05568415ddbda3
pigggyiris <95093482+pigggyiris@users.noreply.github.com>
2023-10-11 13:55:05 -0400

    bubble (#5)

--- src/views/admin/default/components/CampaignIdea.jsx
+++ src/views/admin/default/components/CampaignIdea.jsx
@@ -34,45 +34,58 @@ const CampaignIdea = () => {
       content:
         "Hold a fall foliage photo contest and offer a prize for the best photo. You could also display the photos in your coffee shop.",
     },
-    
   ];
 
   return (
-    <Card extra="!p-[20px] text-left overflow-hidden h-auto">
-      <div className="flex justify-between mb-4">
+    <Card extra="!p-[20px] text-left h-auto">
+      <div className="mb-4 flex justify-between">
         <h4 className="text-xl font-bold text-gray-900 dark:text-white">
           Campaign Idea
         </h4>
       </div>
       <div className="grid grid-rows-2 gap-4">
-  <div className="grid grid-cols-3 gap-4">
-  <div className="col-span-2">
-    <Bubble title={ideas[0].title} content={ideas[0].content} color={ideas[0].color} />
-    </div>
-      <Bubble title={ideas[1].title} content={ideas[1].content} color={ideas[1].color} />
-   
-  </div>
-  <div className="grid grid-cols-5 gap-4">
-    <div className="col-span-2">
-      <Bubble title={ideas[2].title} content={ideas[2].content} color={ideas[2].color} />
-    </div>
-    <div className="col-span-3">
-      <Bubble title={ideas[3].title} content={ideas[3].content} color={ideas[3].color} />
-    </div>
-  </div>
-  <div className="flex justify-center mt-4">
-    <Bubble title={ideas[4].title} content={ideas[4].content} color={ideas[4].color} />
-  </div>
-</div>
-
-
+        <div className="grid grid-cols-4 gap-4">
+          <div className="col-span-2">
+            <Bubble
+              title={ideas[0].title}
+              content={ideas[0].content}
+              color={ideas[0].color}
+            />
+          </div>
+          <div className="col-span-2">
+            <Bubble
+              title={ideas[1].title}
+              content={ideas[1].content}
+              color={ideas[1].color}
+            />
+          </div>
+        </div>
+        <div className="grid grid-cols-7 gap-4">
+          <div className="col-span-3">
+            <Bubble
+              title={ideas[2].title}
+              content={ideas[2].content}
+              color={ideas[2].color}
+            />
+          </div>
+          <div className="col-span-4">
+            <Bubble
+              title={ideas[3].title}
+              content={ideas[3].content}
+              color={ideas[3].color}
+            />
+          </div>
+        </div>
+        <div className="mt-4 flex justify-center">
+          <Bubble
+            title={ideas[4].title}
+            content={ideas[4].content}
+            color={ideas[4].color}
+          />
+        </div>
+      </div>
     </Card>
   );
 };
-
-
-
-
-
 
 export default CampaignIdea;
