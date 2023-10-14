1d7f33d49d0e312476c45e0652ce615c4f914495
wxyn <100791680+XiaoyananWang@users.noreply.github.com>
2023-10-14 19:45:47 -0400

    Bean avatar

--- src/views/admin/default/components/HiringQuestion.jsx
+++ src/views/admin/default/components/HiringQuestion.jsx
@@ -8,7 +8,7 @@ import SearchBar from "./SearchBar";
 
 const HiringQuestion = ({}) => {
   return (
-    <Card>
+    <Card >
       <Chatbot
         config={config}
         messageParser={MessageParser}
