d53caa79bcd9438d4867979bfeb9a8d6fcfb4303
wxyn <100791680+XiaoyananWang@users.noreply.github.com>
2023-10-14 18:10:08 -0400

    Update MarketingPie.jsx

--- src/views/admin/default/components/MarketingPie.jsx
+++ src/views/admin/default/components/MarketingPie.jsx
@@ -1,9 +1,6 @@
 import React, { useState, useEffect } from "react";
 import ReactApexChart from "react-apexcharts";
-import PieChart from "components/charts/PieChart";
-import { pieChartData, pieChartOptions } from "variables/charts";
 import Card from "components/card";
-
 import axios from "axios";
 import { BASE_URL } from "config";
 
@@ -14,7 +11,7 @@ const MarketingPie = () => {
     const fetchData = async () => {
       try {
         const marketingResponse = await axios.get(
-          `${BASE_URL}/v1/salesInfo/marketing_percentage_suggestion`
+          `${BASE_URL}/v1/salesInfo/marketing_percentage_suggestion?interval=monthly`
         );
 
         // Wrap the response data in square brackets if it's not an array
@@ -42,19 +39,22 @@ const MarketingPie = () => {
   const labels = pieData.map((item) => item.marketingMethod);
   console.log("labels", labels);
 
-  const data = pieData.map((item) => item.percentage);
+  const data = pieData.map((item) => parseFloat(item.percentage));
   console.log("data", data);
-  const parsedPercentageData = data.map((data) => {
-    return parseFloat(data);
-  });
-  console.log("parsed percentage data:", parsedPercentageData);
 
   const chartData = {
-    series: parsedPercentageData,
+    series: data,
     labels: labels,
   };
 
-  const colors = ["#6a90a6", "#c69271", "#b4c556", "#dbd3ad", "#fcd34d","#927964"];
+  const colors = [
+    "#6a90a6",
+    "#c69271",
+    "#b4c556",
+    "#dbd3ad",
+    "#fcd34d",
+    "#927964",
+  ];
 
   const chartOptions = {
     chart: {
@@ -72,34 +72,19 @@ const MarketingPie = () => {
       },
     },
     legend: {
-      show: false, 
+      show: false,
     },
   };
 
   console.log("Series:", chartData.series);
 
-  // Create a copy of pieChartOptions and set the labels
-  const updatedPieChartOptions = { ...pieChartOptions };
-  updatedPieChartOptions.labels = labels;
-
   return (
     <Card extra="rounded-[20px] p-3 h-[350px]">
-      <div className="flex flex-row justify-between px-3 pt-2">
-        <div>
-          <h4 className="text-lg font-bold text-navy-700 dark:text-white">
-            Best Marketing Ratio
-          </h4>
-        </div>
+      <h4 className="text-lg font-bold text-navy-700 dark:text-white">
+        Best Marketing Ratio
+      </h4>
 
-        <div className="mb-6 flex items-center justify-center">
-          <select className="mb-3 mr-2 flex items-center justify-center text-sm font-bold text-gray-600 hover:cursor-pointer dark:!bg-navy-800 dark:text-white">
-            <option value="monthly">Monthly</option>
-            <option value="yearly">Daily</option>
-          </select>
-        </div>
-      </div>
-
-      <div className="mb-auto flex h-[220px] w-full items-center justify-center">
+      <div className="mt-3 mb-auto flex h-[220px] w-full items-center justify-center">
         <ReactApexChart
           options={chartOptions}
           series={chartData.series}
