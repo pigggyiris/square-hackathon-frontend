9d42cd3e1a1f811d469e6c78c5107bf52b009af1
pigggyiris <95093482+pigggyiris@users.noreply.github.com>
2023-10-14 21:51:45 -0400

    update flavors

---
+++ src/views/admin/default/components/TopFlavorData.js
@@ -0,0 +1,30 @@
+import Latte from "assets/img/dashboards/Latte.png";
+import ColdBrew from "assets/img/dashboards/ColdBrew.png";
+import Frappuccino from "assets/img/dashboards/Frappuccino.png";
+import Macchiato from "assets/img/dashboards/Macchiato.png";
+import Mocha from "assets/img/dashboards/Mocha.png";
+
+const TopFlavorsData = {
+  "Vanilla Latte": {
+    image: Latte,
+    rating: 5,
+  },
+  "Caramel Macchiato": {
+    image: Macchiato,
+    rating: 5,
+  },
+  Mocha: {
+    image: Mocha,
+    rating: 4,
+  },
+  Frappuccino: {
+    image: Frappuccino,
+    rating: 5,
+  },
+  "Cold Brew": {
+    image: ColdBrew,
+    rating: 4,
+  },
+};
+
+export default TopFlavorsData;
